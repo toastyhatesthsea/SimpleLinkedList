14d64f7e71319b428e854d585e64d1777e1ce83b
Saena <toastyhatesthesea@gmail.com>
2021-07-10 23:58:21 -0700

    Added Push function to simplelinkedList

--- src/main/java/SimpleLinkedList.java
+++ src/main/java/SimpleLinkedList.java
@@ -8,3 +8,91 @@ which is an important part of programming in Java.
 Please remove this comment when submitting your solution.
 
 */
+
+public class SimpleLinkedList<T>
+{
+
+    public int aSize;
+    private Node<T> head;
+    private Node<T> current;
+    private Node<T> previous;
+
+    public <T> SimpleLinkedList()
+    {
+        aSize = 0;
+        head = new Node<>();
+        current = new Node<>();
+        previous = current;
+    }
+
+    public void push(T data)
+    {
+        if (head == null)
+        {
+            head = new Node<>(data, null);
+            current = head;
+            previous = head;
+        } else
+        {
+            if (head == current)
+            {
+                Node<T> nextNode = new Node<>(data, null);
+                head.setNextNode(nextNode);
+                current = nextNode;
+                previous = head;
+            } else
+            {
+                previous = current;
+
+                Node<T> nextNode = new Node<>(data, null);
+                current.setNextNode(nextNode);
+                current = nextNode;
+            }
+        }
+        aSize++;
+    }
+
+    public int size()
+    {
+        return aSize;
+    }
+}
+
+
+class Node<T>
+{
+    private T aData;
+    private Node<T> nextNode;
+
+    public Node(T someData, Node<T> aNode)
+    {
+        this.aData = someData;
+        nextNode = aNode;
+    }
+
+    public Node()
+    {
+        this.aData = null;
+        nextNode = null;
+    }
+
+    public T getData()
+    {
+        return aData;
+    }
+
+    public void setData(T aData)
+    {
+        this.aData = aData;
+    }
+
+    public Node<T> getNextNode()
+    {
+        return nextNode;
+    }
+
+    public void setNextNode(Node<T> nextNode)
+    {
+        this.nextNode = nextNode;
+    }
+}
